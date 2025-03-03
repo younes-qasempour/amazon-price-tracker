f9898df1b387c93defc9b4d5e209c322c832e917
younes_qasempour <qasempouryounes@gmail.com>
2025-03-03 19:52:23 +0330

    Working

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ soup = BeautifulSoup(webpage, "html.parser")
 price = soup.find('span', class_='aok-offscreen').text.strip()
 price = float(price[1:])
 
+message = "Hello"
+
+
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
 smtp.starttls()
-smtp.login('<EMAIL>', '<PASSWORD>')
+smtp.login(user=os.getenv("EMAIL"), password=os.getenv("PASSWORD"))
+smtp.sendmail(
+    from_addr=os.getenv("EMAIL"),
+    to_addrs=os.getenv("TO_EMAIL"),
+    msg=f"subject:HI\n\n{message}"
+)
